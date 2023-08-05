8b6021eccd60940453dbd2ab7649ee3d3eaafae6
ZZKIM <rkgml424@naver.com>
2023-08-05 11:12:26 +0900

    first commit

---
+++ src/main/java/com/trip/demo/WayTripApplication.java
@@ -0,0 +1,13 @@
+package com.trip.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayTripApplication.class, args);
+	}
+
+}
